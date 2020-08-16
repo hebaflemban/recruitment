a573e46f83c840e6fe233e0bf84d02b3b28607aa
Heba <36724067+heba@users.noreply.github.com>
2020-08-16 18:16:35 +0300

    adding the recruitment process

--- recruitment.py
+++ recruitment.py
@@ -1,5 +1,33 @@
 def main():
-	#write your code here
+
+	print("Welcome to the special recruitment program,\n please answer the following questions:")
+	skills = ["HTML", "Java", "C#", "C++", "DotNet", "Python", "Data Structure"]
+	cv ={}
+
+	cv['name'] = input("What's your name? ")
+	cv['age'] = input("How old are you? ")
+	cv['experience'] = input("How many years of experience do you have? ")
+	cv['skills'] = []
+
+	x=1
+	for i in skills:
+		print ("%d.%s" %(x, i))
+		x=x+1
+
+	index1 = input("Choose a skill from above by entering its number: ")
+	index2 = input("Choose another skill from above by entering its number: ")
+
+	cv['skills'].append( skills[int(index1)-1] )
+	cv['skills'].append( skills[int(index2)-1] )
+
+	if int(cv['age'])>24 and int(cv['age'])<41 and int(cv['experience'])>5:
+		if "Python" in cv['skills']:
+			print ("Congratulations %s, you got accepted!" %(cv['name']))
+		else:
+			print("Sorry %s, you got rejected" %(cv['name']))
+	else:
+		print("Sorry %s, you got rejected" %(cv['name']))
+
 
 if __name__ == '__main__':
 	main()
